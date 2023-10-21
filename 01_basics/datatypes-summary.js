e78208b16e0af45ee4c2e64a4345a21e765ae2bf
Aditya narayan yadav <107515156+Naru2002@users.noreply.github.com>
2023-10-21 18:06:21 +0000

    memory-stackandheap

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -35,3 +35,29 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack(primitive datatype), Heap(Non-primitive datatype)
+// In stack variable ki copy di jati hai and in heap reference dete hai
+
+let myyoutubename = "narucodingdotcom"
+let anothername = myyoutubename
+anothername = "narugangsterdotcom"
+
+console.log(myyoutubename)
+console.log(anothername)
+
+let userOne={
+    email: "adityanaru2155@gmail.com",
+    upi: "user@paytm"
+}
+
+let userTwo = userOne
+
+userTwo.email = "naru654523@gmail.com"
+console.log(userOne.email)
+console.log(userTwo.email)
+
